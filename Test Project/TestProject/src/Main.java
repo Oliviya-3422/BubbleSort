181abd81bc678f99892571cdaefc87f776e50651
Amirtha5 <113197577+Amirtha5@users.noreply.github.com>
2023-02-06 12:57:42 +0530

    Update Main.java

--- Test Project/TestProject/src/Main.java
+++ Test Project/TestProject/src/Main.java
@@ -17,7 +17,7 @@ class Main {
 
     }
     public static void main(String[] args) {
-        int arr[] ={3,70,35,2,35,400,5};
+        int arr[] ={3,70,55,2,35,500,5};
 
         System.out.println("Array Before Bubble Sort");
         for(int i=0; i < arr.length; i++){
